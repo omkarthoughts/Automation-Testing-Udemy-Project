8b862a37fec40004d5b70d2c7b2ea39b38ab98a6
Omkar <omkarpatil2552@gmail.com>
2023-06-21 06:45:47 +0530

    Second usere Updated The Project FIles

--- src/intoduction/test.java
+++ src/intoduction/test.java
@@ -2,18 +2,18 @@ package intoduction;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-//public class test {
-//	
-//	public static void main (String []args) {
-//	
-//		System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
-//	WebDriver driver = new ChromeDriver();
-//	driver.get("https://rahulshettyacademy.com");
-//	System.out.println(driver.getTitle());
-//	System.out.println(driver.getCurrentUrl());
-//	driver.close();
-//	}
-//}
+public class test {
+	
+	public static void main (String []args) {
+	
+		System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://rahulshettyacademy.com");
+	System.out.println(driver.getTitle());
+	System.out.println(driver.getCurrentUrl());
+	driver.close();
+	}
+}
 
 class test {
     public static void main(String[] args) {
@@ -39,4 +39,65 @@ class test {
             } 
         }
     }
+}
+public class primeNumFoundber {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        int num = 0;
+
+        int maxCheck = 100; // The limit to find prime numbers is up to 100
+
+        boolean isPrime = true;
+
+        //Empty String
+
+        String primeNumFound = “”;
+
+        //Start loop#2 to maxCheck
+
+        for (i = 2; i <= maxCheck; i++) {
+
+            isPrime = CheckPrime(i);
+
+            if (isPrime) {
+
+             primeNumFound = primeNumFound + i + ” “;
+
+            }
+
+        }
+
+        System.out.println(“Prime numbers from 1 to ” + maxCheck + “:”);
+
+        // It prints prime numbers from 1 to maxCheck
+
+    System.out.println(primeNumFound);
+
+}
+
+    public static boolean CheckPrime(int numToCheck) {
+
+        int remainder;
+
+        for (int i = 2; i <= numToCheck / 2; i++) {
+
+            remainder = numToCheck % i;
+
+            //if remainder gives 0 than numToCheckber is not prime number and breaks loop, else it continues the loop
+
+         if (remainder == 0) {
+
+             return false;
+
+            }
+
+        }
+
+        return true;
+
+}
+
 }
